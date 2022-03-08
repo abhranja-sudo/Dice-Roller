66ccee3854dff0010b43465ccd983b75b85da5e5
ranjan <ar50645@gmail.com>
2022-03-08 00:17:27 -0800

    Menu options logic

--- app/src/main/java/com/zybooks/diceroller/MainActivity.kt
+++ app/src/main/java/com/zybooks/diceroller/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.zybooks.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+const val MAX_DICE = 3
 
 class MainActivity : AppCompatActivity() {
+
+    private var numVisibleDice = MAX_DICE
+    private lateinit var diceList: MutableList<Dice>
+    private lateinit var diceImageViewList: MutableList<ImageView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create list of Dice
+        diceList = mutableListOf()
+        for (i in 0 until MAX_DICE) {
+            diceList.add(Dice(i + 1))
+        }
+
+        // Create list of ImageViews
+        diceImageViewList = mutableListOf(
+            findViewById(R.id.dice1), findViewById(R.id.dice2), findViewById(R.id.dice3))
+
+        showDice()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Determine which menu option was chosen
+        return when (item.itemId) {
+            R.id.action_one -> {
+                changeDiceVisibility(1)
+                showDice()
+                true
+            }
+            R.id.action_two -> {
+                changeDiceVisibility(2)
+                showDice()
+                true
+            }
+            R.id.action_three -> {
+                changeDiceVisibility(3)
+                showDice()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun changeDiceVisibility(numVisible: Int) {
+        numVisibleDice = numVisible
+
+        // Make dice visible
+        for (i in 0 until numVisible) {
+            diceImageViewList[i].visibility = View.VISIBLE
+        }
+
+        // Hide remaining dice
+        for (i in numVisible until MAX_DICE) {
+            diceImageViewList[i].visibility = View.GONE
+        }
+    }
+
+    private fun showDice() {
+
+        // Show visible dice
+        for (i in 0 until numVisibleDice) {
+            val diceDrawable = ContextCompat.getDrawable(this, diceList[i].imageId)
+            diceImageViewList[i].setImageDrawable(diceDrawable)
+            diceImageViewList[i].contentDescription = diceList[i].imageId.toString()
+        }
     }
 }
